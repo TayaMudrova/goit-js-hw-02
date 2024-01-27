ef632d56bc96f2d8467cecb5638ab03479792e31
Taya Mudrova <113426294+TayaMudrova@users.noreply.github.com>
2024-01-27 20:22:46 +0200

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,16 @@
+let content = '20px';
+let padding = '5px';
+let border = '0px';
+
+function getElementWidth(content, padding, border) {
+  content = Number.parseFloat(content);
+  padding = Number.parseFloat(padding);
+  border = Number.parseFloat(border);
+  return content + (padding + border) * 2;
+}
+
+getElementWidth();
+
+console.log(getElementWidth('50px', '8px', '4px')); // 74
+console.log(getElementWidth('60px', '12px', '8.5px')); // 101
+console.log(getElementWidth('200px', '0px', '0px')); // 200
